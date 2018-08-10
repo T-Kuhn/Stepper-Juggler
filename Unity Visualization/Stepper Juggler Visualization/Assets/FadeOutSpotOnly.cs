2f9c19f38db44e19810a524f0502dc24b0cf8119
T-Kuhn <tkuhn.mpu@gmail.com>
2018-08-10 10:42:49 +0900

    new visualization with green dots.

---
+++ Unity Visualization/Stepper Juggler Visualization/Assets/FadeOutSpotOnly.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class FadeOutSpotOnly : MonoBehaviour
+{
+    [SerializeField] private MeshRenderer meshRenderer;
+
+    void Start()
+    {
+        meshRenderer.material.DOColor(new Color(0f, 0f, 0f, 0f), "_TintColor", 10f);
+    }
+}
